2d3d26d64377a65c95579d4e4146e3020f3b256e
Joel Purra <code+github@joelpurra.com>
2013-12-01 18:04:35 +0100

    Some jslinting

--- src/pagehasformchanges.joelpurra.js
+++ src/pagehasformchanges.joelpurra.js
@@ -1,84 +1,77 @@
 /*!
-* @license PageHasFormChanges
-* Copyright © 2012, 2013 Joel Purra <http://joelpurra.com/>
-* Released under MIT, BSD and GPL license. Comply with at least one.
-*
-* https://github.com/joelpurra/pagehasformchanges
-*
-* A jQuery plugin to check if *anything* has changed in *any form* on a page,
-* and warn the user before leaving the page.
-*/
+ * @license PageHasFormChanges
+ * Copyright © 2012, 2013 Joel Purra <http://joelpurra.com/>
+ * Released under MIT, BSD and GPL license. Comply with at least one.
+ *
+ * https://github.com/joelpurra/pagehasformchanges
+ *
+ * A jQuery plugin to check if *anything* has changed in *any form* on a page,
+ * and warn the user before leaving the page.
+ */
 
-/*jslint vars: true, white: true, browser: true*/
-/*global jQuery*/
+/*jslint white: true */
+/*global window: true, document: true, JoelPurra: true, debug: true, jQuery: true */
 
 // Set up namespace, if needed
 var JoelPurra = JoelPurra || {};
 
-(function ($, namespace)
-{
-   namespace.PageHasFormChanges = {};
+(function($, namespace) {
+    "use strict";
 
-    var defaultOptions = {
-        leavingPageWarningMessage: "Changes have been detected in the form. Leaving the page will discard all form input.",
-        resetWarningOnPreventedSubmit: false
-    }
+    var tag = "PageHasFormChanges",
 
-    var options = $.extend(true, {}, defaultOptions);
+        ns = namespace.PageHasFormChanges = {},
 
-    var userHasChangedSomething = false;
+        defaultOptions = {
+            leavingPageWarningMessage: "Changes have been detected in the form. Leaving the page will discard all form input.",
+            resetWarningOnPreventedSubmit: false
+        },
 
-    var tag = "PageHasFormChanges";
+        options = $.extend(true, {}, defaultOptions),
 
-    $(document).change(function (e)
-    {
+        userHasChangedSomething = false,
+
+        $document = $(document);
+
+    $document.change(function(e) {
         debug.log(tag, "$(document).change() detected", e);
 
-        if (userHasChangedSomething !== true)
-        {
+        if (userHasChangedSomething !== true) {
             debug.info(tag, "Triggered by change event. Will warn user before leaving the page.");
         }
 
         userHasChangedSomething = true;
     });
 
-    $(document).submit(function (e)
-    {
+    $document.submit(function(e) {
         debug.log(tag, "$(document).submit() detected", e);
 
-        if(e.isDefaultPrevented())
-        {
+        if (e.isDefaultPrevented()) {
             debug.log(tag, "$(document).submit() event was already prevented from actually submitting the form.", e);
 
-            if(options.resetWarningOnPreventedSubmit !== true)
-            {
+            if (options.resetWarningOnPreventedSubmit !== true) {
                 return;
             }
         }
 
-        if (userHasChangedSomething === true)
-        {
+        if (userHasChangedSomething === true) {
             debug.info(tag, "Reset by submit event. Will not warn user before leaving the page.");
         }
 
         userHasChangedSomething = false;
     });
 
-    function initializeOnBeforeUnloadListener()
-    {
+    function initializeOnBeforeUnloadListener() {
         // From http://jonathonhill.net/2011-03-04/catching-the-javascript-beforeunload-event-the-cross-browser-way/
-        window.onbeforeunload = function (e)
-        {
-            if (userHasChangedSomething !== true)
-            {
+        window.onbeforeunload = function(e) {
+            if (userHasChangedSomething !== true) {
                 return null;
             }
 
-            var e = e || window.event;
+            e = e || window.event;
 
             // For IE and Firefox prior to version 4
-            if (e)
-            {
+            if (e) {
                 e.returnValue = options.leavingPageWarningMessage;
             }
 
@@ -87,17 +80,15 @@ var JoelPurra = JoelPurra || {};
         };
     }
 
-    namespace.PageHasFormChanges.setOptions = function (userOptions)
-    {
+    ns.setOptions = function(userOptions) {
         var optionsBefore = options;
 
         options = $.extend(true, {}, options, userOptions);
 
         debug.log(tag, "setOptions", optionsBefore, userOptions, options);
-    }
+    };
 
-    function atStartup()
-    {
+    function atStartup() {
         initializeOnBeforeUnloadListener();
     }
 
